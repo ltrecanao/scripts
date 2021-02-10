14310ecb7989e7ec54147fd8bc0b7f4f3d738a71
ltrecanao <ltrecanao@protonmail.com>
2021-02-10 18:25:47 -0300

    Script create

---
+++ fedora/rpm-repos.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# install free and nonfree rpmfusion repositories
+yum -y install \
+https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
