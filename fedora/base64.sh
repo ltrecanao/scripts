7f530a83b5cde21d0f7f7daa71f2eafa2edc8c67
Lucas Trecanao <76416245+ltrecanao@users.noreply.github.com>
2021-03-18 20:49:54 -0300

    Create base64.sh

---
+++ fedora/base64.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+while [[ -n "$1" ]]; do
+        case "$1" in
+                -e|--encode)
+                        echo -n $2 | base64
+                        shift ;;
+                -d|--decode)
+                        echo -n $2 | base64 -d
+                        shift ;;
+                *) echo 'Use the -e and -d parameters to encode and decode respectively!' ;;
+        esac
+        shift
+done
